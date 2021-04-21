80dd6364948151bd1a02f7d084daf20407882ca9
Thales Dias <thalesod@hotmail.com>
2021-04-21 01:08:07 -0300

    Update NLW Day 2 (part.1)

--- plantmanager/App.tsx
+++ plantmanager/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Welcome } from './src/pages/Welcom';
+import { Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+
+
+import { UserIdentification } from './src/pages/UserIndetification';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
 
 export default function App(){
+  const [ fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded)
+    return <AppLoading />
+
   return (
-    <Welcome />
+    <UserIdentification />
   )
 }
--- plantmanager/src/pages/Welcom.tsx
+++ plantmanager/src/pages/Welcom.tsx
@@ -1,27 +1,46 @@
 import React from 'react';
-import { SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import { 
+    SafeAreaView,
+    View, 
+    Text, 
+    Image, 
+    TouchableOpacity, 
+    StyleSheet,
+    Dimensions,
+
+} from 'react-native';
+import { Feather } from '@expo/vector-icons'
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
-import {Button} from '../components/button'
+import fonts from '../styles/fonts'
 
 export function Welcome(){
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil
-            </Text>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de{'\n'}
+                    de forma fácil
+                </Text>
 
-            <Image source={wateringImg} style={styles.image}/>
+                <Image 
+                    source={wateringImg} 
+                    style={styles.image}
+                    resizeMode="contain"/>
 
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
 
-            <Button />
-
+                <TouchableOpacity style={styles.button} activeOpacity={0.4}>
+                    <Feather
+                        name="chevron-right"
+                        style={styles.buttonIcon}>
+                    </Feather>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -29,21 +48,32 @@ export function Welcome(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    wrapper: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
+        
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
         marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    image: {
+        width: Dimensions.get('window').width * 0.7
     },
     button:{
         backgroundColor: colors.green,
@@ -54,12 +84,8 @@ const styles = StyleSheet.create({
         height: 56,
         width: 56
     },
-    image: {
-        width: 292,
-        height: 284,
-    },
-    buttonText: {
-        color: colors.white,
-        fontSize: 24,
+    buttonIcon: {
+        fontSize: 32,
+        color: colors.white
     }
 })
